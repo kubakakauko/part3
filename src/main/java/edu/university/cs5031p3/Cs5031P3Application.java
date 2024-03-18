3e47f460b5c85a77a4854f3454338f468a87f987
kubakakauko <47721520+kubakakauko@users.noreply.github.com>
2024-03-18 17:57:28 +0000

    Initial commit

---
+++ src/main/java/edu/university/cs5031p3/Cs5031P3Application.java
@@ -0,0 +1,13 @@
+package edu.university.cs5031p3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs5031P3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs5031P3Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/university/cs5031p3/Cs5031P3ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.university.cs5031p3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs5031P3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
